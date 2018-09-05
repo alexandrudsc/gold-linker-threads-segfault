faf0db8c5701a8ee95fb18532ada931152991268
Alexandru Dascalu <adascalu@computervoice.ro>
2018-09-05 18:04:16 +0300

    updated sh scripts

--- run-docker-container.sh
+++ run-docker-container.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-docker run -it --rm=true --name=Test -v $(pwd):/root/test_gold_linker -w /root/test_gold_linker debian_latest_binutils bash compile-and-link.sh
+docker run -it --rm=true --name=Test -v $(pwd):/root/test_gold_linker -w /root/test_gold_linker debian_latest_binutils
 
